67c2a019a55fa3023d9f6a80ec421a04e3bafacd
JumyazovMahmud <mahmudjumyazov919@gmail.com>
2026-08-30 00:24:31 +0500

    Final Futledge

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "app.makhmudjon.internglobe"
+    namespace = "app.makhmudjon.futledge"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -33,7 +33,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "app.makhmudjon.internglobe"
+        applicationId = "app.makhmudjon.futledge"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
